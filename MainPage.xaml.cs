0aea91fe03024f9b89cbcdb6baeb54c68802da00
David Lenihan <alpha@lenihan.xyz>
2017-03-21 00:22:38 -0700

    Playing with keyboard input

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI;
 using System.Numerics;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Effects;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -29,6 +30,29 @@ namespace _3dmath
         public MainPage()
         {
             this.InitializeComponent();
+            System.Diagnostics.Debug.WriteLine("TESTING!!!!!!!!");
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += onAcceleratorKey;
+
+            // TODO: Not sure which keyboard handler to use....ALT doesn't go to keyup/keydown.
+            //       acceleratorKey catches alt...but doesn't behave same as keyup/keydown...more info.
+
+        }
+
+        private void onAcceleratorKey(CoreDispatcher sender, AcceleratorKeyEventArgs args)
+        {
+            System.Diagnostics.Debug.WriteLine($"AccelKey: {args.VirtualKey.ToString()}");
+        }
+
+        private void CoreWindow_KeyUp(CoreWindow sender, KeyEventArgs args)
+        {
+            System.Diagnostics.Debug.WriteLine($"KeyUp: {args.VirtualKey.ToString()}");
+        }
+
+        private void CoreWindow_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            System.Diagnostics.Debug.WriteLine($"KeyDown: {args.VirtualKey.ToString()}");
         }
 
         private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
