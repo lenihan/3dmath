20326bf82fdc317f40c698e6032858cd1bab5324
David Lenihan <alpha@lenihan.xyz>
2017-03-23 09:45:59 -0700

    Reading shift, alt, and ctrl

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,6 +3,7 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI;
 using Windows.UI.Core;
 using System.Diagnostics;
+using Windows.System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -13,24 +14,44 @@ namespace _3dmath
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        bool shiftDown = false;
+        bool altDown = false;
+        bool ctrlDown = false;
         public MainPage()
         {
             this.InitializeComponent();
             Debug.WriteLine("TESTING!!!!!!!!");
-            Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += onAcceleratorKey;
+            Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += OnAcceleratorKey;
 
             // TODO: Not sure which keyboard handler to use....ALT doesn't go to keyup/keydown.
             //       acceleratorKey catches alt...but doesn't behave same as keyup/keydown...more info.
 
         }
 
-        private void onAcceleratorKey(CoreDispatcher sender, AcceleratorKeyEventArgs args)
+        private void OnAcceleratorKey(CoreDispatcher sender, AcceleratorKeyEventArgs args)
         {
-            Debug.WriteLine($"AccelKey: {args.VirtualKey}, Event: {args.EventType}, IsExtendedKey: {args.KeyStatus.IsExtendedKey}, IsKeyReleased: {args.KeyStatus.IsKeyReleased}, IsMenuKeyDown: {args.KeyStatus.IsMenuKeyDown}, RepeatCount: {args.KeyStatus.IsMenuKeyDown}, ScanCode: {args.KeyStatus.ScanCode}, WasKeyDown: {args.KeyStatus.WasKeyDown}");
+            bool isKeyDown = !args.KeyStatus.IsKeyReleased;
+            if (args.VirtualKey == VirtualKey.Shift)
+            {
+                shiftDown = isKeyDown;
+            }
+            else if (args.VirtualKey == VirtualKey.Menu)
+            {
+                altDown = isKeyDown;
+            }
+            else if (args.VirtualKey == VirtualKey.Control)
+            {
+                ctrlDown = isKeyDown;
+            }
+            else if (args.EventType == CoreAcceleratorKeyEventType.KeyDown ||
+                     args.EventType == CoreAcceleratorKeyEventType.SystemKeyDown)
+            {
+                Debug.WriteLine($"AccelKey: {args.VirtualKey}, Event: {args.EventType}, shiftDown: {shiftDown},  ctrlDown: {ctrlDown},  altDown: {altDown}, IsExtendedKey: {args.KeyStatus.IsExtendedKey}, IsKeyReleased: {args.KeyStatus.IsKeyReleased}, IsMenuKeyDown: {args.KeyStatus.IsMenuKeyDown}, RepeatCount: {args.KeyStatus.IsMenuKeyDown}, ScanCode: {args.KeyStatus.ScanCode}, WasKeyDown: {args.KeyStatus.WasKeyDown}");
+            }
         }
           
 
-        private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
+        private void Canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
         {
             args.DrawingSession.DrawText("Hello, World!", 100, 100, Colors.Black);
         }
